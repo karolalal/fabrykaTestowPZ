2b056dc03724d5eb22d5b67113b13dff03ee2f1e
karolalal <karolina.x.banach@gmail.com>
2022-09-13 19:27:32 +0200

    iframe 6.6

--- scripts/6.6IFrame.py
+++ scripts/6.6IFrame.py
@@ -4,16 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-
 driver.get('https://simpletestsite.fabrykatestow.pl/')
-
 driver.maximize_window()
 
 iframe = driver.find_element(By.XPATH, "//div[@id='iframe-header']")
 iframe.click()
 
 driver.switch_to.frame(driver.find_element(By.TAG_NAME, 'iframe'))
-
 driver.find_element(By.ID, 'simpleButton1').click()
 
 htmlelement= driver.find_element(By.TAG_NAME, 'html')
